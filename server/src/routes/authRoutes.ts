cb6b6746240d0dc285ddbdd7e2d850aa579bf9ea
Srimanta Jana <srimantajana@live.com>
2025-11-17 21:41:09 -0800

    Test host.docker.internal mapping

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -137,3 +137,4 @@ router.post('/verify-email', async (req: Request, res: Response, next: NextFunct
 // Final test of dynamic testing
 // Testing intelligent duplicate detection - Mon, Nov 17, 2025  9:24:16 PM
 // Testing Docker networking fix with host.docker.internal
+// Testing host.docker.internal mapping
