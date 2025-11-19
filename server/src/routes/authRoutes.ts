e7bde0aaf1004a2534a269a8aa950efa9bbb1b37
Srimanta Jana <srimantajana@live.com>
2025-11-18 19:32:10 -0800

    testing

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -133,5 +133,3 @@ router.post('/verify-email', async (req: Request, res: Response, next: NextFunct
 // Test API detection - Mon, Nov 17, 2025  5:12:41 PM
 // Testing improved duplicate detection - $(date)
 // Test change 
-// test change
-// test2
